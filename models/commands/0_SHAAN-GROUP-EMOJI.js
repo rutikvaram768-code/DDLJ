50dd68a39781ef8057f416d7b3c9086e241af385
rutikvaram768-code <rutikvaram768@gmail.com>
2026-02-22 16:53:43 +0530

    Update 0_SHAAN-GROUP-EMOJI.js

--- models/commands/0_SHAAN-GROUP-EMOJI.js
+++ models/commands/0_SHAAN-GROUP-EMOJI.js
@@ -2,8 +2,8 @@ module.exports.config = {
 	name: "groupemoji",
 	version: "1.0.0", 
 	hasPermssion: 0,
-	credits: "SHAAN BABU",
-	description: "THIS BOT WAS MADE BY MR SHAAN BABU",
+	credits: "RUTIK BABU",
+	description: "THIS BOT WAS MADE BY MR RUTIK BABU",
 	commandCategory: "CHANGE GROUP EMOJI", 
 	usages: "PREFIX", 
 	cooldowns: 0,
@@ -13,5 +13,5 @@ module.exports.config = {
 module.exports.run = async function({ api, event, args }) {
 	var emoji = args.join(" ")
 	if (!emoji) api.sendMessage("BOSS SAATH ME EK EMOJI DALO JO LAGANA HA 😐✌️", event.threadID, event.messageID)
-	else api.changeThreadEmoji(emoji, event.threadID, () => api.sendMessage(`BOSS MAINE GROUP KA EMOJI BADAL DIYA 👉 ${emoji}\n━━━━━━━━━━━━━━━━━━━━━━━\nOWNER  𒁍 MR SHAAN BABU 🌺`, event.threadID, event.messageID));
+	else api.changeThreadEmoji(emoji, event.threadID, () => api.sendMessage(`BOSS MAINE GROUP KA EMOJI BADAL DIYA 👉 ${emoji}\n━━━━━━━━━━━━━━━━━━━━━━━\nOWNER  𒁍 MR ✮⃝❤≛⃝ 𝐑𝐔𝐓𝐈𝐊──────亗🕊️ BABU 🌺`, event.threadID, event.messageID));
 }
